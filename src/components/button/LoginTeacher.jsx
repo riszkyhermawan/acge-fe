09cf35fc4e2f8601ad3a8459f3f83d6eb6e441c0
Vulbough <71969296+riszkyhermawan@users.noreply.github.com>
2025-10-20 02:55:36 +0700

    (feat) Login and Register feature
    - Update `AuthContext,jsx` to  have login, register, getMe function
    - Update `api.js` to get all the API related to login/register feature from backend

---
+++ src/components/button/LoginTeacher.jsx
@@ -0,0 +1,12 @@
+const LoginTeacher = () => {
+    return (
+      <>
+        <div className="w-1/3 mt-8 p-4 rounded-full shadow-md inline-flex justify-between items-center cursor-pointer border-2">
+          <p className="text-center mx-auto font-bold text-xl">Login as Teacher</p>
+        </div>
+      </>
+    );
+}
+
+
+export default LoginTeacher;
---
+++ src/pages/landing/Home.jsx
@@ -0,0 +1,32 @@
+import bg from "./../../assets/img/bg-home.webp";
+import hero from "./../../assets/img/hero-home.webp";
+import ButtonStudent from "./../../components/button/GettingStarted.jsx";
+import ButtonTeacher from "./../../components/button/LoginTeacher.jsx";
+
+const Home = () => {
+  return (
+    <div
+      className="h-screen w-screen flex flex-row items-center justify-center bg-cover"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div className="w-7xl flex items-center justify-between ">
+        <div className="w-full h-fit flex flex-col pr-7">
+          <h1 className="text-white font-bold text-6xl  ">
+            Tackling Coding Assignments, At ease.
+          </h1>
+          <div className="w-full space-between flex flex-row items-center gap-4">
+            <ButtonStudent />
+            <ButtonTeacher />
+          </div>
+          
+        </div>
+
+        <div className="w-full h-fit">
+          <img src={hero} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
